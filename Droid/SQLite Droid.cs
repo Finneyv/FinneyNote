56cd1b7c2b5e2185f7e56b8aa876fafce2b40341
Victor Finney <finneyv09@gmail.com>
2017-06-07 08:40:59 -0400

    Added Droid functionality

---
+++ Droid/SQLite Droid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using FinneyNote.Droid;
+using SQLite;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SQLite_Droid))]
+namespace FinneyNote.Droid
+{
+	public class SQLite_Droid : ISQlite
+	{
+		public SQLite_Droid()
+		{
+		}
+		public SQLiteConnection GetConnection()
+		{
+			var sqliteFilename = "FinneyNote.db";
+			string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			//string libraryPath = Path.Combine(documentsPath, "..", "Library");
+			var path = Path.Combine(documentsPath, sqliteFilename);
+			File.Open(path, FileMode.OpenOrCreate);
+			var conn = new SQLiteConnection(path);
+			return conn;
+		}
+	}
+}
+
